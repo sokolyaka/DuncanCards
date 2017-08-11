0e124606c8c9da5d9154c3d378f6fee7319d7c20
Oleh Sokolov <O.sokolov@newline.tech>
2017-08-11 16:11:16 +0300

    refactored CardsActivity to fullscreen

--- app/src/main/java/sokolov/dunkancards/cards/view/CardsActivity.java
+++ app/src/main/java/sokolov/dunkancards/cards/view/CardsActivity.java
@@ -1,43 +1,24 @@
 package sokolov.dunkancards.cards.view;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import sokolov.dunkancards.R;
+import sokolov.dunkancards.util.HideSystemNavigation;
 
 public class CardsActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cards);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        SectionsPagerAdapter mSectionsPagerAdapter = new SectionsPagerAdapter(
-                getSupportFragmentManager());
+        new HideSystemNavigation(getWindow().getDecorView()).hide();
 
-        ViewPager mViewPager = (ViewPager) findViewById(R.id.container);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
-
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                Snackbar
-                        .make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null)
-                        .show();
-            }
-        });
-
+        ((ViewPager) findViewById(R.id.container))
+                .setAdapter(
+                        new SectionsPagerAdapter(
+                                getSupportFragmentManager()));
     }
 }
