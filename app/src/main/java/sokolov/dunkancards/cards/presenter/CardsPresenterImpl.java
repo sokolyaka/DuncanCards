6ad03e3d22f6c51f002c25647f8d150eccf94d48
sokolyaka <sokolov.ob@gmail.com>
2017-08-14 21:54:59 +0300

    onCreate test

--- app/src/main/java/sokolov/dunkancards/cards/presenter/CardsPresenterImpl.java
+++ app/src/main/java/sokolov/dunkancards/cards/presenter/CardsPresenterImpl.java
@@ -1,15 +1,19 @@
 package sokolov.dunkancards.cards.presenter;
 
 import sokolov.dunkancards.cards.interactor.CardsInteractor;
-import sokolov.dunkancards.cards.view.CardsActivity;
 import sokolov.dunkancards.cards.view.CardsView;
 
 public class CardsPresenterImpl implements CardsPresenter {
+    private final CardsView cardsView;
+    private final CardsInteractor cardsInteractor;
+
     public CardsPresenterImpl(CardsView cardsView, CardsInteractor cardsInteractor) {
+        this.cardsView = cardsView;
+        this.cardsInteractor = cardsInteractor;
     }
 
     @Override
     public void onCreate() {
-
+        cardsView.showCards(null);
     }
 }
---
+++ app/src/test/java/sokolov/dunkancards/cards/presenter/CardsPresenterImplTest.java
@@ -0,0 +1,35 @@
+package sokolov.dunkancards.cards.presenter;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Answers;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import sokolov.dunkancards.cards.interactor.CardsInteractor;
+import sokolov.dunkancards.cards.view.CardsView;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+public class CardsPresenterImplTest {
+
+    private CardsPresenterImpl cardsPresenter;
+    @Mock(answer = Answers.RETURNS_MOCKS)
+    private CardsView cardsView;
+    @Mock(answer = Answers.RETURNS_MOCKS)
+    private CardsInteractor cardsInteractor;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        cardsPresenter = new CardsPresenterImpl(cardsView, cardsInteractor);
+    }
+
+    @Test
+    public void testOnCreate() {
+        cardsPresenter.onCreate();
+        verify(cardsView, times(1)).showCards(null);
+    }
+
+}
