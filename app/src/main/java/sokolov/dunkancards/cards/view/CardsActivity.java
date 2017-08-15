b5853c537d067c7f7059ab208b0a6a8210e60ef2
sokolyaka <sokolov.ob@gmail.com>
2017-08-15 14:30:56 +0300

    hided status bar in cards activity

--- app/src/main/java/sokolov/dunkancards/cards/view/CardsActivity.java
+++ app/src/main/java/sokolov/dunkancards/cards/view/CardsActivity.java
@@ -3,16 +3,17 @@ package sokolov.dunkancards.cards.view;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 
 import java.util.List;
 
 import sokolov.dunkancards.DuncanCardsApp;
 import sokolov.dunkancards.R;
 import sokolov.dunkancards.cards.interactor.CardsInteractorImpl;
+import sokolov.dunkancards.cards.model.CardsInMemoryRepository;
 import sokolov.dunkancards.cards.presenter.CardsPresenter;
 import sokolov.dunkancards.cards.presenter.CardsPresenterImpl;
-import sokolov.dunkancards.cards.model.CardsInMemoryRepository;
-import sokolov.dunkancards.util.HideSystemNavigation;
 import sokolov.dunkancards.util.ZoomOutPageTransformer;
 
 public class CardsActivity extends AppCompatActivity implements CardsView {
@@ -20,12 +21,9 @@ public class CardsActivity extends AppCompatActivity implements CardsView {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_cards);
-
-        new HideSystemNavigation(
-                getWindow()
-                        .getDecorView())
-                .hide();
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         CardsPresenter cardsPresenter =
                 new CardsPresenterImpl(
