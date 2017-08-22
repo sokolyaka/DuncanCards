7ab129edbb2ca9da9d3532ba3b4e4e17b497d213
sokolyaka <sokolov.ob@gmail.com>
2017-08-23 00:02:30 +0300

    impl categories constants in CardsInMemoryRep

--- app/src/main/java/sokolov/dunkancards/cards/model/CardsInMemoryRepository.java
+++ app/src/main/java/sokolov/dunkancards/cards/model/CardsInMemoryRepository.java
@@ -1,7 +1,10 @@
 package sokolov.dunkancards.cards.model;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import static sokolov.dunkancards.categories.model.CategoriesConstants.TRANSPORT_ID;
 
 public class CardsInMemoryRepository implements CardsRepository {
 
@@ -10,21 +13,27 @@ public class CardsInMemoryRepository implements CardsRepository {
     public CardsInMemoryRepository() {
         models =
                 Arrays.asList(
-                        new CardModelImpl(0, 1, "transport/truck.jpg", "Truck"),
-                        new CardModelImpl(1, 1, "transport/ambulance.jpg", "Ambulance"),
-                        new CardModelImpl(2, 1, "transport/auto.jpg", "Auto"),
-                        new CardModelImpl(3, 1, "transport/bicycle.jpg", "Bicycle"),
-                        new CardModelImpl(4, 1, "transport/bus.jpg", "Bus"),
-                        new CardModelImpl(5, 1, "transport/motorcycle.jpg", "Motorcycle"),
-                        new CardModelImpl(6, 1, "transport/police.jpg", "Police"),
-                        new CardModelImpl(7, 1, "transport/tram.jpg", "Tram"),
-                        new CardModelImpl(8, 1, "transport/trolleybus.jpg", "Trolleybus"),
-                        new CardModelImpl(9, 1, "transport/fire truck.jpg", "Fire truck")
+                        new CardModelImpl(0, TRANSPORT_ID, "transport/truck.jpg", "Truck"),
+                        new CardModelImpl(1, TRANSPORT_ID, "transport/ambulance.jpg", "Ambulance"),
+                        new CardModelImpl(2, TRANSPORT_ID, "transport/auto.jpg", "Auto"),
+                        new CardModelImpl(3, TRANSPORT_ID, "transport/bicycle.jpg", "Bicycle"),
+                        new CardModelImpl(4, TRANSPORT_ID, "transport/bus.jpg", "Bus"),
+                        new CardModelImpl(5, TRANSPORT_ID, "transport/motorcycle.jpg", "Motorcycle"),
+                        new CardModelImpl(6, TRANSPORT_ID, "transport/police.jpg", "Police"),
+                        new CardModelImpl(7, TRANSPORT_ID, "transport/tram.jpg", "Tram"),
+                        new CardModelImpl(8, TRANSPORT_ID, "transport/trolleybus.jpg", "Trolleybus"),
+                        new CardModelImpl(9, TRANSPORT_ID, "transport/fire truck.jpg", "Fire truck")
                 );
     }
 
     @Override
     public List<CardModel> getCardsByCategoryId(int id) {
-        return (List<CardModel>) models;
+        List<CardModel> result = new ArrayList<>();
+        for (CardModel model : models) {
+            if (model.categoryId() == id) {
+                result.add(model);
+            }
+        }
+        return result;
     }
 }
