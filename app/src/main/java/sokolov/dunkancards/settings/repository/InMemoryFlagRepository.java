76b80744a7cef1b7c249f3bd42ca792c7935c486
sokolyaka <sokolov.ob@gmail.com>
2017-08-25 12:42:40 +0300

    content for flag rep

--- app/src/main/java/sokolov/dunkancards/settings/repository/InMemoryFlagRepository.java
+++ app/src/main/java/sokolov/dunkancards/settings/repository/InMemoryFlagRepository.java
@@ -1,9 +1,21 @@
 package sokolov.dunkancards.settings.repository;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class InMemoryFlagRepository implements FlagRepository {
+
+    private static final Map<String, String> PATHS;
+
+    static {
+        PATHS = new HashMap<>();
+        PATHS.put("en", "flags/en.svg");
+        PATHS.put("ua", "flags/ua.svg");
+        PATHS.put("ru", "flags/ru.svg");
+    }
 
     @Override
     public String getPathByShortName(String languageShortName) {
-        return null;
+        return PATHS.get(languageShortName);
     }
 }
